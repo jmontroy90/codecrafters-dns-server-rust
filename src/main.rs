a8f7512cd97891077c9682f0928a638e306a5eb6
John Montroy <jmontroy90@gmail.com>
2025-04-08 12:20:12 -0400

    feat: Write header section

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,47 @@
 #[allow(unused_imports)]
 use std::net::UdpSocket;
+use bytes::{Bytes, BufMut, BytesMut};
+
+struct Header {
+    packet_identifier: u16,
+    query_response_indicator: bool,
+    operation_code: u8,
+    authoritative_answer: bool,
+    truncation: bool,
+    recursion_desired: bool,
+    recursion_available: bool,
+    reserved: u8,
+    response_code: u8,
+    question_count: u16,
+    answer_record_count: u16,
+    authority_record_count: u16,
+    additional_record_count: u16,
+}
+
+
+impl Header {
+    pub fn to_bytes(&self) -> Bytes {
+        let mut buf = BytesMut::with_capacity(96);
+        buf.put_u16(self.packet_identifier);
+        buf.put_u8(
+            (self.query_response_indicator as u8) << 7
+                | (self.operation_code) << 3
+                | (self.authoritative_answer as u8) << 2
+                | (self.truncation as u8) << 1
+                | (self.recursion_desired as u8),
+        );
+        buf.put_u8(
+            (self.recursion_available as u8) << 7
+            | (self.reserved) << 4
+            | (self.response_code) << 2
+        );
+        buf.put_u16(self.question_count);
+        buf.put_u16(self.answer_record_count);
+        buf.put_u16(self.authority_record_count);
+        buf.put_u16(self.additional_record_count);
+        buf.freeze()
+    }
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -9,13 +51,30 @@ fn main() {
     let udp_socket = UdpSocket::bind("127.0.0.1:2053").expect("Failed to bind to address");
     let mut buf = [0; 512];
 
+    let response = Header {
+        packet_identifier: 1234,
+        query_response_indicator: true,
+        operation_code: 0,
+        authoritative_answer: false,
+        truncation: false,
+        recursion_desired: false,
+        recursion_available: false,
+        reserved: 0,
+        response_code: 0,
+        question_count: 0,
+        answer_record_count: 0,
+        authority_record_count: 0,
+        additional_record_count: 0,
+    };
+
+    // eprintln!("{:?}\n", &response.to_bytes());
+
     loop {
         match udp_socket.recv_from(&mut buf) {
             Ok((size, source)) => {
                 println!("Received {} bytes from {}", size, source);
-                let response = [];
                 udp_socket
-                    .send_to(&response, source)
+                    .send_to(&response.to_bytes(), source)
                     .expect("Failed to send response");
             }
             Err(e) => {
