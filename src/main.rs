6a422bbab0a9a8a2703c998bb2ec12cde43958e1
John Montroy <jmontroy90@gmail.com>
2025-04-11 11:16:57 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,7 @@ fn main() {
     loop {
         match udp_socket.recv_from(&mut bs) {
             Ok((size, source)) => {
-                let mut buf = BytesMut::from(&bs[..size]);
-                println!("JOHN: Pre trim: {:?}", buf);
-                trim_excess_nulls(&mut buf);
-                println!("JOHN: Post trim: {:?}", buf);
+                let buf = BytesMut::from(&bs[..size]);
                 let resp = build_response(buf);
                 println!("Received {} bytes from {}", size, source);
                 udp_socket
@@ -26,16 +23,6 @@ fn main() {
             }
         }
     }
-}
-
-// This is AI, sorryyyyy
-fn trim_excess_nulls(buf: &mut BytesMut) {
-    let mut last_non_null = buf.len();
-    while last_non_null > 0 && buf[last_non_null - 1] == 0 {
-        last_non_null -= 1;
-    }
-    let new_len = (last_non_null + 1).min(buf.len());
-    buf.truncate(new_len);
 }
 
 fn build_response(mut buf: BytesMut) -> BytesMut {
